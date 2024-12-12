0aa555bc5acaaf8bb159be1afe82e06c32d3602d
dimazardawan <116993004+dimazardawan@users.noreply.github.com>
2024-12-12 23:14:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const chatbotRoutes = require('./routes/chatbotRoutes');
 const app = express();
 app.use(express.json());
 
+// // Tambahkan route untuk endpoint '/'
+// app.get('/', (req, res) => {
+//     res.send('Testing Cloud Run, by CC Bangkit!');
+// });
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/history', historyRoutes);
@@ -17,6 +22,22 @@ app.use('/api/scan', scanRoutes);
 app.use('/api/news', newsRoutes);
 app.use('/api/chatbot', chatbotRoutes);
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the API!',
+        endpoints: [
+            '/api/auth',
+            '/api/users',
+            '/api/history',
+            '/api/scan',
+            '/api/news',
+            '/api/chatbot'
+        ]
+    });
+});
+
 // Port diambil dari .env
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
